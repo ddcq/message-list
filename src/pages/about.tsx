5f77123dd9accac0c1910d9153d05b7723c565e2
Denis DECLERCQ <forhimandus@gmail.com>
2020-09-16 15:51:55 +0200

    feat: clean about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react'
-import { Text, TextContainer } from 'react-md'
+import { Link, Text, TextContainer } from 'react-md'
 
 export default function Contact(): ReactElement {
   return (
@@ -9,49 +9,37 @@ export default function Contact(): ReactElement {
       <Text type="headline-6">Mail</Text>
       <p>declercq.denis@free.fr</p>
       <Text type="headline-6">Around the word</Text>
-      <ul className="list-inline around-web">
+      <ul>
         <li>
-          <a href="http://ca.linkedin.com/in/denis-declercq-bb6b298" className="btn-social">
-            <span className="title">LinkedIn</span>
-            <span className="icon"> <i className="fa fa-fw fa-linkedin"></i> </span>
-          </a>
+          <Link href="http://ca.linkedin.com/in/denis-declercq-bb6b298">
+            LinkedIn
+          </Link>
         </li>
         <li>
-          <a href="http://www.viadeo.com/p/0021epf6mkaedrhk" className="btn-social">
-            <span className="title">Viadeo</span>
-            <span className="icon"> <i className="fa fa-fw fa-viadeo"></i> </span>
-          </a>
+          <Link href="http://www.viadeo.com/p/0021epf6mkaedrhk">
+            Viadeo
+          </Link>
         </li>
         <li>
-          <a href="http://plus.google.com/u/0/+DenisDECLERCQ" className="btn-social">
-            <span className="title">Google+</span>
-            <span className="icon"> <i className="fa fa-fw fa-google"></i> </span>
-          </a>
+          <Link href="http://plus.google.com/u/0/+DenisDECLERCQ">
+            Google+
+          </Link>
         </li>
       </ul>
       <Text type="headline-6">CV à télécharger</Text>
-      <ul className="list-inline resume">
+      <ul>
         <li>
-          <a href="resume/CV - Denis Declercq.docx" className="btn-social">
-            <span className="description"><i className="fa fa-file-word-o"></i> Word</span>
-          </a>
+          <Link href="https://ddcq.github.io/resume/CV - Denis Declercq.docx" className="btn-social">
+            Word
+          </Link>
         </li>
         <li>
-          <a href="resume/CV - Denis Declercq - light.docx" className="btn-social">
-            <span className="description"><i className="fa fa-file-word-o"></i> Word (version
-										light)</span>
-          </a>
+          <Link href="https://ddcq.github.io/resume/CV - Denis Declercq - light.docx" className="btn-social">
+            Word (version light)
+          </Link>
         </li>
       </ul>
-      <div className="footer-below">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              Copyright © Denis Declercq 2020
-					</div>
-          </div>
-        </div>
-      </div>
+      Copyright © Denis Declercq 2020
     </TextContainer>
   )
 }
