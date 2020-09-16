8c275df2bc7281654d12fd8f422c85b9541222ae
Denis DECLERCQ <forhimandus@gmail.com>
2020-09-16 08:22:57 +0200

    feat: favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -31,6 +31,10 @@ class MyApp extends App<AppInitialProps> {
       <Layout>
         <Head>
           <title>Denis Declercq - Message lList for LeBonCoin</title>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,500:700&display=swap" />
         </Head>
