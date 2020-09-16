32abb0206c959c90ff076af0c076e297bf93422a
Denis DECLERCQ <forhimandus@gmail.com>
2020-09-16 09:19:26 +0200

    feat: add logger on every reducer action

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,29 @@
-import thunk from 'redux-thunk'
-import { Context, createWrapper, MakeStore } from 'next-redux-wrapper'
-import { applyMiddleware, createStore } from 'redux'
-// import { devToolsEnhancer } from 'redux-devtools-extension'
-import rootReducer from './root-reducer'
-import RootState from './root-state'
+import { Context, createWrapper, MakeStore } from 'next-redux-wrapper';
+import { applyMiddleware, createStore, Middleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './root-reducer';
+import RootState from './root-state';
+
+const logger: Middleware = ({ getState }) => {
+  return next => action => {
+    console.log('will dispatch', action)
+
+    // Call the next dispatch method in the middleware chain.
+    const returnValue = next(action)
+
+    console.log('state after dispatch', getState())
+
+    // This will likely be the action itself, unless
+    // a middleware further in chain changed it.
+    return returnValue
+  }
+}
 
 // create a makeStore function
 const makeStore: MakeStore<RootState> = (_context: Context) => createStore(
   rootReducer,
-  applyMiddleware(thunk)
+  composeWithDevTools(applyMiddleware(thunk, logger))
 );
 
 // export an assembled wrapper
